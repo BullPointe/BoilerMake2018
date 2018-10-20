c896654d72c3387486da4d180e4f449b7175d74f
Sahil Jaganmohan <dev.sahil.jaganmohan@gmail.com>
2018-10-20 05:05:33 -0400

    Fitbit Connection
    Connected to Fitbit API and gets BPM

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
 const express = require('express')
+var request = require('request');  
 const app = express()
 const port = 3000
 
-// app.get('/', (req, res) => res.redirect('./index.html'))
-app.get('/', (req, res) => res.sendFile('public/index.html', { root: __dirname }))
+function getUserBPM(){
 
-var requestURL = 'https://api.fitbit.com/1/user/6XYDTQ/activities/heart/date/today/1d.json';
-var XMLHttpRequest= require("xmlhttprequest").XMLHttpRequest;
-var request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
+  // app.get('/getBPM', (req, res) => {
+  // "Request" library
+  var client_id = '22D8PZ'; // Your client id
+  var client_secret = '4a14d64fdf640e48deb2e0df100ed3c2'; // Your secret
 
-request.onload = function() 
-{
-  var fitBitInformation = request.response;
-  console.log(fitBitInformation)
-  app.get('/', (req, res) => res.send(fitBitInformation["activities-heart"][0]["restingHeartRate"]))
-}
+  // your application requests authorization
+  var authOptions = {
+    url: 'https://api.fitbit.com/oauth2/token',
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+    },
+    form: {
+      grant_type: 'client_credentials'
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+
+      // use the access token to access the Spotify Web API
+      var token = body.access_token;
+      token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkQ4UFoiLCJzdWIiOiI2WFlEVFEiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd3BybyB3bnV0IHdzbGUgd3dlaSB3c29jIHdhY3Qgd3NldCB3bG9jIiwiZXhwIjoxNTQwNjI0MTg4LCJpYXQiOjE1NDAwMjI2OTV9.RJtGOrvdvoF25BrZeP8Wo8WWs1sN7qDr5k0-bEYENJI'
+      var options = {
+        url: 'https://api.fitbit.com/1/user/6XYDTQ/activities/heart/date/today/1d.json',
+        headers: {
+          'Authorization': 'Bearer ' + token
+        },
+        json: true
+      };
+      request.get(options, function(error, response, body) {
+        console.log(body);
+        json = body['activities-heart-intraday']['dataset']
+        result = json[json.length-1]['value']
+        res.send(JSON.stringify(result)) 
+      });
+    }
+  });
+
+};
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
