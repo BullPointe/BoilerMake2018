65e76c64e8d05646327ca19f3ec7e7241f00c0fa
18vijayb <18vijayb@students.harker.org>
2018-10-20 00:55:42 -0400

    Added HeartRate API

--- app.js
+++ app.js
@@ -4,4 +4,19 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+var requestURL = 'https://api.fitbit.com/1/user/6XYDTQ/activities/heart/date/today/1d.json';
+var XMLHttpRequest= require("xmlhttprequest").XMLHttpRequest;
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() 
+{
+  var fitBitInformation = request.response;
+  console.log(fitBitInformation)
+  app.get('/', (req, res) => res.send(fitBitInformation["activities-heart"][0]["restingHeartRate"]))
+}
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
