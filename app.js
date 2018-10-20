61b95caa1f0d1b9b1d88e0b1b9d897ac301b91bd
Sahil Jaganmohan <dev.sahil.jaganmohan@gmail.com>
2018-10-20 00:49:46 -0400

    Public Directory Init
    Public/index.html

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.redirect('./index.html'))
+app.get('/', (req, res) => res.sendFile('public/index.html', { root: __dirname }))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
